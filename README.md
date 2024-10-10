572126f2380685cac10830a301557562b9f2640b
Jaden Lim <jadenlimjc@gmail.com>
2024-10-10 14:08:27 +0800

    removed excess files

--- README.md
+++
@@ -1,2 +0,0 @@
-# ee2026_project
-repo to contain source files for ee2026 project
--- xy_convert.v
+++
@@ -1,4 +0,0 @@
-module xy_convert(input [12:0] pixel_index, output [7:0] x, output [6:0] y);
-assign x = pixel_index % 96;
-assign y = pixel_index / 96;
-endmodule
